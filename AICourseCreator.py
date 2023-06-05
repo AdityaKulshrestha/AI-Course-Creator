d68251bd8a7722e26f007f1362192f51ccaa69de
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-06-05 16:17:20 +0530

    Improved the prompt

--- AICourseCreator.py
+++ AICourseCreator.py
@@ -9,7 +9,6 @@ import os
 import streamlit as st
 
 
-
 def content(title):
     with open('content_template.txt', 'r') as f:
         template = f.read()
@@ -22,16 +21,16 @@ def content(title):
         return chain.run(title), cb
 
 
-def image_request(caption):
+def image_request(caption, topic):
     search = GoogleSerperAPIWrapper(type="images")
-    results = search.results(caption.group()[1:-1],image_size='small')
-    # print(search.results('Photosynthesis'))
+    results = search.results(caption.group()[1:-1] + " " + topic + ' imagesize:300x300', imgSize='small')
+    print(results)
     return f"![{caption.group()[1:-1]}]({results['images'][0]['imageUrl']})"
 
 
 # Replace image description with image link
-def link_to_image(response):
-    response = re.sub(r'\<(.*?)\>', lambda match: image_request(match), response)
+def link_to_image(response, title):
+    response = re.sub(r'\<(.*?)\>', lambda match: image_request(match, title), response)
     return response
 
 
@@ -43,14 +42,12 @@ serpapi_key = st.text_input("Enter your SerperAPI key", type='password')
 os.environ['OPENAI_API_KEY'] = openai_key
 os.environ['SERPER_API_KEY'] = serpapi_key
 
-
 title = st.text_input("Enter the title of your course")
-
 
 if st.button("Submit"):
     response, tokens = content(title)
     # st.text("Tokens used: {}".format(tokens))
     # print(response)
-    response = link_to_image(response)
+    response = link_to_image(response, title)
     # st.text(response)
     st.markdown(response)
